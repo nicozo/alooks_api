2b52877283fb6a4d5d4e622ad605205843142eca
nicozo <niczo.soccer161@gmail.com>
2022-03-23 01:15:36 +0900

    Change: ユーザー登録用のコントローラを変更

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,17 +1,2 @@
 class Api::V1::UsersController < ApplicationController
-  def create
-    user = User.new(user_params)
-
-    if user.save
-      render json: user
-    else
-      render json: user.errors, status: :bad_request
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
 end
