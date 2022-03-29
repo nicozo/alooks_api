839f57cd11e4fb3ad883c7525bb9b4048379bd27
nicozo <niczo.soccer161@gmail.com>
2022-03-29 17:22:47 +0900

    Add: user_idを暗号化

--- app/services/user_auth/refresh_token.rb
+++ app/services/user_auth/refresh_token.rb
@@ -9,24 +9,34 @@ module UserAuth
     def initialize(user_id: nil, token: nil)
       if token.present?
         @token = token
-        @payload = JWT.decode(@token, secret_key, true, verify_claims).first
+        @payload = JWT.decode(@token.to_s, secret_key, true, verify_claims).first
         @user_id = get_user_id_from(@payload)
       else
-        @user_id = user_id
+        @user_id = encrypt_for(user_id)
+        # @user_id = user_id
         @payload = claims
         @token = JWT.encode(@payload, secret_key, algorithm, header_fields)
         remember_jti(user_id)
       end
     end
 
+    def entity_for_user(user_id)
+      id ||= @user_id
+      User.find(decrypt_for(id))
+    end
+
+    # def entity_for_user(user_id)
+    #   User.find(user_id)
+    # end
+
     private
 
-    def refresh_token_life
+    def token_life
       UserAuth.refresh_token_life
     end
 
-    def refresh_token_expiration
-      refresh_token_life.from_now.to_i
+    def token_expiration
+      token_life.from_now.to_i
     end
 
     def jwt_id
@@ -51,7 +61,7 @@ module UserAuth
 
     def verify_jti?(jti, payload)
       user_id = get_user_id_from(payload)
-      decode_user = user_id
+      decode_user = entity_for_user(user_id)
       decode_user.refresh_jti == jti
     rescue ActiveRecord::RecordNotFound
       false
@@ -68,5 +78,3 @@ module UserAuth
     end
   end
 end
-
-
--- app/services/user_auth/token_commons.rb
+++ app/services/user_auth/token_commons.rb
@@ -8,8 +8,31 @@ module UserAuth
       UserAuth.token_algorithm
     end
 
+    def user_claim
+      UserAuth.user_claim
+    end
+
     def get_user_id_from(payload)
       payload[user_claim]
+    end
+
+    def crypt
+      salt = "signed user id"
+      key_length = ActiveSupport::MessageEncryptor.key_len
+      secret = Rails.application.key_generator.generate_key(salt, key_length)
+      ActiveSupport::MessageEncryptor.new(secret)
+    end
+
+    def encrypt_for(user_id)
+      return unless user_id
+      crypt.encrypt_and_sign(user_id.to_s, purpose: :authorization)
+    end
+
+    def decrypt_for(user_id)
+      return unless user_id
+      crypt.decrypt_and_verify(user_id.to_s, purpose: :authorization)
+    rescue
+      nil
     end
 
     def header_fields
@@ -20,4 +43,3 @@ module UserAuth
     end
   end
 end
-
