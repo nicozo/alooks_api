a5c318cd423501c4a469e6e286bb3cf28d55d6ab
nicozo <niczo.soccer161@gmail.com>
2022-03-29 14:00:21 +0900

    Add: ユーザーモデルにカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_012551) do
+ActiveRecord::Schema.define(version: 2022_03_29_045033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_03_16_012551) do
     t.integer "role", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "refresh_jti"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
