1d22c00ef790026a7448b35d4d490b3669603e7a
nicozo <niczo.soccer161@gmail.com>
2022-04-05 18:49:53 +0900

    Improve: roomsスキーマを変更

--- db/schema.rb
+++ db/schema.rb
@@ -18,10 +18,12 @@ ActiveRecord::Schema.define(version: 2022_04_05_093935) do
   create_table "rooms", force: :cascade do |t|
     t.string "title", null: false
     t.integer "current_squad_member", null: false
-    t.boolean "is_draft"
+    t.boolean "is_draft", default: false
     t.datetime "application_deadline"
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_04_05_093935) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "rooms", "users"
 end
