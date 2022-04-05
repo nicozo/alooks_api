5985a3913f54b5498fafa9cdcf7a599803fcdffd
nicozo <niczo.soccer161@gmail.com>
2022-04-05 22:08:15 +0900

    Add: ダミーデータを作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do 
+  user = User.create!(
+    email: Faker::Internet.unique.safe_email,
+    password: 'password',
+    password_confirmation: 'password',
+    name: Faker::Name.unique.name
+  )
+end
+
+10.times do |n|
+  room = Room.create!(
+    title: "Apex部屋#{n + 1}",
+    current_squad_member: rand(1..2),
+    application_deadline: rand(1..2).hour.from_now,
+    user_id: User.find(n + 1).id,
+  )
+end
