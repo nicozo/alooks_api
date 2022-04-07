e8d207c62ec8cca4b84951361c7b5436d3f8866b
nicozo <niczo.soccer161@gmail.com>
2022-04-07 15:54:36 +0900

    Add: Platformテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_05_093935) do
+ActiveRecord::Schema.define(version: 2022_04_07_065338) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "platforms", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.string "title", null: false
