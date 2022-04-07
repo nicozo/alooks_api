66b97d334dd9a020e510874c8ccbcf82d70b69ca
nicozo <niczo.soccer161@gmail.com>
2022-04-07 21:57:49 +0900

    Add: GameModeテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_07_070930) do
+ActiveRecord::Schema.define(version: 2022_04_07_125454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "game_modes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "platforms", force: :cascade do |t|
     t.string "name"
---
+++ spec/factories/game_modes.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :game_mode do
+    name { "MyString" }
+  end
+end
