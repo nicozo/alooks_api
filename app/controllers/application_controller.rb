cd70592a9a1314c63b8a3d97b3b69dcf7e38168f
nicozo <niczo.soccer161@gmail.com>
2022-04-09 17:16:56 +0900

    Update: application_controller.rbでCSRF対策を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,16 @@ class ApplicationController < ActionController::API
   include ActionController::Cookies
   include UserAuthenticate
   include TokenGenerate
+
+  # CSRF対策
+  before_action :xhr_in_request_header?
+
+  private
+
+  def xhr_in_request_header?
+    # リクエストヘッダ内にX-Requested-With: 'XMLHttpRequest'の存在を判定
+    return if request.xhr?
+    # なければ403エラーを返す
+    render status: :forbidden, json: { status: 403, error: "Forbidden" }
+  end
 end
