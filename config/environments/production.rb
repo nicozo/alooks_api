e52379e39d599d0a9554bdb20d601ea6b2f569af
nicozo <niczo.soccer161@gmail.com>
2022-07-28 22:19:34 +0900

    Fix: herokuデプロイ後のエラー対策でproduction.rbにhostの設定を追加

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,8 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # Missing host to link to! Please provide the :host parameter, set default_url_options[:host], or set :only_path to true (ArgumentError)エラーの対策
+  Rails.application.routes.default_url_options[:host] = 'localhost'
+  Rails.application.routes.default_url_options[:port] = 3000
 end
