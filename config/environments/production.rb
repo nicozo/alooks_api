eddf0cbe21c8b3921b97cf35e0c58e99e1020079
nicozo <niczo.soccer161@gmail.com>
2022-07-28 23:08:39 +0900

    Fix: default_url_options[:host]に渡す値を環境変数から文字列に変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,5 +112,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   
-  Rails.application.routes.default_url_options[:host] = ENV['DOMAIN']
+  Rails.application.routes.default_url_options[:host] = 'https://alooks-api.herokuapp.com'
 end
