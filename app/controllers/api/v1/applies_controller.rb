93b52e2fde65f6b21cfd2833fa4eecc86b8d73f2
nicozo <niczo.soccer161@gmail.com>
2022-08-06 15:38:47 +0900

    Add: コントローラを作成

--- app/controllers/api/v1/applies_controller.rb
+++ app/controllers/api/v1/applies_controller.rb
@@ -1,10 +1,35 @@
 class Api::V1::AppliesController < ApplicationController
   def index
+    @applies = Apply.to_host.order(created_at: :desc)
+
+    render json: @applies
   end
 
   def create
+    @apply = Apply.new(apply_params)
+    @apply.host_id = @apply.room.user_id
+
+    if @apply.save
+      render json: @apply
+    else
+      render json: @apply.errors, status: :bad_request
+    end
   end
 
   def destroy
+    # @apply = Apply.where(user_id: params[:user_id], room_id: params[:room_id])
+    @apply = Apply.record_exist?(apply_params)
+    @apply.destroy
+
+    render json: @apply, status: ok
+  end
+
+  private
+
+  def apply_params
+    params.require(:apply).permit(
+      :user_id,
+      :room_id
+    )
   end
 end
--- app/models/apply.rb
+++ app/models/apply.rb
@@ -5,4 +5,7 @@ class Apply < ApplicationRecord
   validates :body, presence: true, length: { maximum: 25 }
   validates :user_id, uniqueness: { scope: :room_id }
   validates :host_id, presence: true
+
+  scope :to_host, -> { where(host_id: current_user.id) }
+  scope :record_exist?, -> (params) { where(user_id: params[:user_id], room_id: params[:room_id]) }
 end
