2520ed60d0fa5cf03059ad4eeb701f5ada4702ac
nicozo <niczo.soccer161@gmail.com>
2022-08-06 14:06:59 +0900

    Add: Applyyテーブルにhost_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_02_075940) do
+ActiveRecord::Schema.define(version: 2022_08_06_050433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2022_08_02_075940) do
     t.bigint "room_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "host_id"
     t.index ["room_id"], name: "index_applies_on_room_id"
     t.index ["user_id", "room_id"], name: "index_applies_on_user_id_and_room_id", unique: true
     t.index ["user_id"], name: "index_applies_on_user_id"
