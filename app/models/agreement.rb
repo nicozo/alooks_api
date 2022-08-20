eeb97d263c849345f526cf746a6580a87541abba
nicozo <niczo.soccer161@gmail.com>
2022-08-20 15:12:43 +0900

    Update: バリデーションを追加

--- app/models/agreement.rb
+++ app/models/agreement.rb
@@ -1,4 +1,17 @@
 class Agreement < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+  validates :body, presence: true, length: { maximum: 25 }
+  validates :user_id, uniqueness: { scope: :applicant_id }
+  validates :room_id, uniqueness: { scope: :applicant_id }
+  validates :applicant_id, presence: true
+
+  def applicant
+    user.response_json
+  end
+
+  def agreed_room
+    room.response_json
+  end
 end
