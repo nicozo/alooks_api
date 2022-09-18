02a0cd2167d64420eec88ef9818abd264e2bfd56
nicozo <niczo.soccer161@gmail.com>
2022-09-19 00:03:32 +0900

    Update: 題名を修正

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
-
   def reset_password_email(user)
     @user = User.find user.id
     @url = ENV['API_DOMAIN'] + "/reset_password/update?id=" + @user.reset_password_token
-    mail(:to => user.email,
-         :subject => "Your password has been reset")
+    mail(:to => @user.email,
+         :subject => "【エペスク】パスワードの変更をしてください")
   end
 end
