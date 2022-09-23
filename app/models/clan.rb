0e8236e179ddfb5e4a39eff47b46872be9deec69
nicozo <niczo.soccer161@gmail.com>
2022-09-23 16:13:41 +0900

    Update: バリデーションを追加

--- app/models/clan.rb
+++ app/models/clan.rb
@@ -1,3 +1,8 @@
 class Clan < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :concept, presence: true, length: { maximum: 100 }
+  validates :interview, presence: true, length: { maximum: 100 }
+  validates :prohibited_matters, presence: true, length: { maximum: 100 }
 end
