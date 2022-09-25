bb2d94736e41255a20c585aecd4362b3306f3063
nicozo <niczo.soccer161@gmail.com>
2022-09-26 00:54:28 +0900

    Update: indexアクションとcreateアクションを追加

--- app/controllers/api/v1/clans_controller.rb
+++ app/controllers/api/v1/clans_controller.rb
@@ -1,11 +1,21 @@
 class Api::V1::ClansController < ApplicationController
   def index
+    @clans = Clan.all.includes(association_tables).order(created_at: :desc)
+
+    render json: @clans.as_json(methods: %i[host])
   end
 
   def show
   end
 
   def create
+    @clan = current_user.clans.build(clan_params)
+
+    if @clan.save
+      render json: @clan
+    else
+      render json: @clan.errors, status: :bad_request
+    end
   end
 
   def edit
@@ -15,5 +25,14 @@ class Api::V1::ClansController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+
+  def association_tables
+    %i[user]
+  end
+
+  def clan_params
   end
 end
