5bdada25653d447659376efd37a69c3a75619556
nicozo <niczo.soccer161@gmail.com>
2022-09-26 01:03:08 +0900

    Clean: infromationsテーブルを削除

--- app/models/information.rb
+++ app/models/information.rb
@@ -1,9 +1,9 @@
-class Information < ApplicationRecord
-  belongs_to :clan
+# class Information < ApplicationRecord
+#   belongs_to :clan
 
-  validates :enrollment, presence: true, length: { maximum: 25 }
-  validates :activity_time, presence: true, length: { maximum: 25 }
-  validates :enrollment_age, presence: true, length: {  maximum: 25}
-  validates :snipe, presence: true, length: { maximum: 35 }
-  validates :contact_means, presence: true, length: { maximum: 25 }
-end
+#   validates :enrollment, presence: true, length: { maximum: 25 }
+#   validates :activity_time, presence: true, length: { maximum: 25 }
+#   validates :enrollment_age, presence: true, length: {  maximum: 25}
+#   validates :snipe, presence: true, length: { maximum: 35 }
+#   validates :contact_means, presence: true, length: { maximum: 25 }
+# end
