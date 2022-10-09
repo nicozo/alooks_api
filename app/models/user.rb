53e82bdac7d0a744bba203529cc533e6dc97d442
nicozo <niczo.soccer161@gmail.com>
2022-10-09 23:59:00 +0900

    Update: パスワードのバリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   has_one :clan, dependent: :destroy
   has_one_attached :avatar
 
-  validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, length: { minimum: 7 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
