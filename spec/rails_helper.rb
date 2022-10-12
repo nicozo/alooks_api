ddc525c9dc9a4236658966b749ac1f58c9c6fe66
nicozo <niczo.soccer161@gmail.com>
2022-10-12 20:47:43 +0900

    Add: roomsリクエストスペックを作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -65,4 +65,5 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include ResponseHelper
   config.include ActiveSupport::Testing::TimeHelpers
+  config.include LoginMarcos
 end
--- spec/requests/api/v1/rooms_spec.rb
+++ spec/requests/api/v1/rooms_spec.rb
@@ -1,52 +1,140 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Rooms", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/api/v1/rooms/index"
-      expect(response).to have_http_status(:success)
-    end
+  let(:user) { create(:user) }
+  let(:headers) { { CONTENT_TYPE: 'application/json', ACCEPT: 'application/json', Authorization: "Bearer #{@access_token}" } }
+  let(:password) { 'password' }
+  let(:params) { { email: user.email, password: password }.to_json }
+
+  def login_process
+    login(params)
+    @access_token = body['token']
   end
 
-  describe "GET /new" do
-    it "returns http success" do
-      get "/api/v1/rooms/new"
-      expect(response).to have_http_status(:success)
+  describe "GET /index" do
+    let(:room_num) { 10 }
+    let(:http_request) { get api_v1_rooms_path, headers: headers, xhr: true }
+
+    before do
+      login_process
+      create_list(:room, room_num, user: user)
+    end
+
+    context "with access_token" do
+      it "returns rooms in json format" do
+        http_request
+
+        expect(body.count).to eq(room_num)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
   describe "GET /show" do
-    it "returns http success" do
-      get "/api/v1/rooms/show"
-      expect(response).to have_http_status(:success)
+    let(:room) { create(:room, user: user) }
+    let(:http_request) { get api_v1_room_path(room.id), headers: headers, xhr: true }
+
+    before do
+      login_process
+    end
+
+    context "with access_token" do
+      it "returns rooms in json format" do
+        http_request
+
+        expect(body['id']).to eq(room.id)
+        expect(body['title']).to eq(room.title)
+        expect(body['platform']).to eq(room.platform)
+        expect(body['game_mode']).to eq(room.game_mode)
+        expect(body['rank_tier']).to eq(room.rank_tier)
+        expect(body['recruitment_number']).to eq(room.recruitment_number)
+        expect(body['user_id']).to eq(user.id)
+        expect(body['host']['id']).to eq(user.id)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
   describe "GET /create" do
-    it "returns http success" do
-      get "/api/v1/rooms/create"
-      expect(response).to have_http_status(:success)
+    let(:http_request) { get api_v1_rooms, headers: headers, xhr: true }
+
+    before do
+      login_process
+    end
+
+    context "with access_token" do
+      it "returns rooms in json format" do
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
   describe "GET /edit" do
-    it "returns http success" do
-      get "/api/v1/rooms/edit"
-      expect(response).to have_http_status(:success)
+    let(:room) { create(:room, user: user) }
+    let(:http_request) { get edit_api_v1_room_path(room.id), headers: headers, xhr: true }
+
+    before do
+      login_process
+    end
+
+    context "with access_token" do
+      it "returns rooms in json format" do
+        http_request
+
+        expect(body['id']).to eq(room.id)
+        expect(body['title']).to eq(room.title)
+        expect(body['platform']).to eq(room.platform)
+        expect(body['game_mode']).to eq(room.game_mode)
+        expect(body['rank_tier']).to eq(room.rank_tier)
+        expect(body['recruitment_number']).to eq(room.recruitment_number)
+        expect(body['user_id']).to eq(user.id)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
   describe "GET /update" do
-    it "returns http success" do
-      get "/api/v1/rooms/update"
-      expect(response).to have_http_status(:success)
+    let(:room) { create(:room, user: user) }
+
+    before do
+      login_process
+    end
+
+    context "with access_token" do
+      let(:http_request) { patch api_v1_room_path(room.id), headers: headers, params: room_params, xhr: true }
+      let(:updated_title) { 'updated title' }
+      let(:room_params) { attributes_for(:room, title: updated_title, application_deadline: 60).to_json }
+
+      it "returns rooms in json format" do
+        http_request
+
+        expect(body['title']).to eq(updated_title)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
   describe "GET /destroy" do
-    it "returns http success" do
-      get "/api/v1/rooms/destroy"
-      expect(response).to have_http_status(:success)
+    let!(:room) { create(:room, user: user) }
+
+    before do
+      login_process
+    end
+
+    context "with access_token" do
+      let(:http_request) { delete api_v1_room_path(room.id), headers: headers, xhr: true }
+
+      it "returns rooms in json format" do
+        http_request
+
+        expect(user.rooms).not_to include(room)
+        expect(response).to be_successful
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 
