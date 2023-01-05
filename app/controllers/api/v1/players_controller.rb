c430949014ea63a8241f3d4e8b67a782b06d50b7
nicozo <niczo.soccer161@gmail.com>
2023-01-05 21:54:24 +0900

    Update: 削除機能を追加

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::PlayersController < ApplicationController
-  before_action :set_player, only: %i[show]
+  before_action :set_player, only: %i[show edit update destroy]
   before_action :current_user_profile_completed?, only: %i[create]
   skip_before_action :authenticate_user, only: %i[index show]
 
@@ -7,7 +7,8 @@ class Api::V1::PlayersController < ApplicationController
     players = Player.all.includes(association_tables).order(created_at: :desc)
 
     render json: players.as_json(
-      only: %i[id body], methods: %i[host]
+      only: %i[id body user_id],
+      methods: %i[host]
     )
   end
 
@@ -25,6 +26,9 @@ class Api::V1::PlayersController < ApplicationController
   end
 
   def destroy
+    @player.destroy!
+
+    render json: @player
   end
 
   private
